2206c97ab3b87873e6f2ae50b1e67b43dc364b76
nagat6h <nagats3do@gmail.com>
2025-11-27 19:35:17 +0300

    حل تكليف بايثون

---
+++ backend/app.py
@@ -0,0 +1,22 @@
+# قائمة المنتجات والأسعار
+products = [ ["منتج 1", 10],
+             ["منتج 2", 20],
+             ["منتج 3", 30],
+             ["منتج 4", 40],
+             ["منتج 5", 50]]
+
+# حلقة لا نهائية حتى يدخل رقم صحيح
+while True: 
+# عرض قائمة المنتجات
+    print("اختر رقم المنتج:")
+    for i in range(len(products)):
+     print(f"{i+1} - {products[i][0]}")
+
+    productNumber = int(input("اكتب الرقم: "))
+    if 1 <= productNumber <= 5:
+        price = products[productNumber-1][1]
+        total_price = price + (price * 0.15)
+        print(f"سعر المنتج شامل الضريبة: {total_price} ريال")
+        break  # نخرج من الحلقة بعد إدخال صحيح
+    else:
+        print("رقم المنتج غير صحيح، حاول مرةش أخرى.\n")
