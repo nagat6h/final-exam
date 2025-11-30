e715124019214857cb494229031b9d1b4478e4e7
nagat6h <nagats3do@gmail.com>
2025-11-30 12:12:20 +0300

    مشروع الـ  (HTML + CSS & Bootstrap)

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,13 @@
-# قائمة المنتجات والأسعار
+
 products = [ ["منتج 1", 10],
              ["منتج 2", 20],
              ["منتج 3", 30],
              ["منتج 4", 40],
              ["منتج 5", 50]]
 
-# حلقة لا نهائية حتى يدخل رقم صحيح
+
 while True: 
-# عرض قائمة المنتجات
+
     print("اختر رقم المنتج:")
     for n in range(len(products)):
      print(f"{n+1} - {products[n][0]}")
@@ -17,6 +17,6 @@ while True:
         price = products[productNumber-1][1]
         total_price = price + (price * 0.15)
         print(f"سعر المنتج شامل الضريبة: {total_price} ريال")
-        break  # نخرج من الحلقة بعد إدخال صحيح
+        break 
     else:
         print("رقم المنتج غير صحيح، حاول مرةش أخرى.\n")
